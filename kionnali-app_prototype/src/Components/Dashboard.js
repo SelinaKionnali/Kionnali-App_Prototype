f0447af46b5d16a7083e158f43d71a381fb4058b
Selina Lavery <selina118@hotmail.com>
2023-11-06 17:15:31 +0000

    Dashboard component background

--- kionnali-app_prototype/src/Components/Dashboard.js
+++ kionnali-app_prototype/src/Components/Dashboard.js
@@ -1,4 +1,19 @@
 // Desc: Dashboard component
+import "./Dashboard.css";
+import { useHistory } from "react-router-dom"; // if using react-router
+
 export default function Dashboard() {
-	return <div className="dashboard">Dashboard</div>;
+	let history = useHistory(); // if using react-router
+
+	return (
+		<>
+			<div className="dashboard">
+				<div className="rounded-tile">
+					<div className="coffee-tile"></div>
+					<div className="weather-tile"></div>
+					<div className="flag-tile"></div>
+				</div>
+			</div>
+		</>
+	);
 }
