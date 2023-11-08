a30d972b3bf64fead75cb0b713343a206c905d71
Selina Lavery <selina@kionnali.com>
2023-11-08 12:00:37 +0000

    Full update button now expands over the content below

--- kionnali-app_prototype/src/Components/Dashboard.js
+++ kionnali-app_prototype/src/Components/Dashboard.js
@@ -26,47 +26,56 @@ export default function Dashboard() {
 					<div className="off-grid"></div>
 
 					<div className="rounded-tile">
-						<button class="weather-button"></button>
-						<button class="coffee-button"></button>
-						<button class="flag-button"></button>
-						<div className="roamstead-status-box">
-							<div className="sunflower-icon"></div>
-							<h4>Your Roamstead is looking good today.</h4>
-							<button
-								className={`full-update-btn ${
-									isExpanded ? "full-update-expanded" : ""
-								}`}
-								onClick={handleToggle}>
-								{isExpanded ? (
-									<>
-										{expandedContent}
-										<br /> <br /> <br />
-										<button
-											className="tell-me-more"
-											onClick={handleHighlight}>
-											<span>Tell me more &gt;</span>
-										</button>
-										<button className="close-button">
-											<span>X</span>
-										</button>
-									</>
-								) : (
-									<span>Full Update</span>
-								)}
-							</button>
-						</div>
-						<div className="system-status-frame">
-							<div className="system-status-box">
-								<button className="power-system-tile"></button>
+						<button
+							className="weather-button"
+							aria-label="Weather Information"></button>
+						<button
+							className="coffee-button"
+							aria-label="Morning Mode"></button>
+						<button
+							className="flag-button"
+							aria-label="Preparation"></button>
+
+						<div className="roamstead-status-container">
+							<div className="roamstead-status-box">
+								<div className="sunflower-icon"></div>
+								<h4>Your Roamstead is looking good today.</h4>
+								<button
+									className={`full-update-btn ${
+										isExpanded ? "full-update-expanded" : ""
+									}`}
+									onClick={handleToggle}>
+									{isExpanded ? (
+										<>
+											{expandedContent}
+											<br /> <br /> <br />
+											<button
+												className="tell-me-more"
+												onClick={handleHighlight}>
+												<span>Tell me more &gt;</span>
+											</button>
+											<button className="close-button">
+												<span>X</span>
+											</button>
+										</>
+									) : (
+										<span>Full Update</span>
+									)}
+								</button>
 							</div>
-							<div className="system-status-box">
-								<button className="thermal-system-tile"></button>
-							</div>
-							<div className="system-status-box">
-								<button className="water-system-tile"></button>
-							</div>
-							<div className="system-status-box">
-								<button className="compost-system-tile"></button>
+							<div className="system-status-frame">
+								<div className="system-status-box">
+									<button className="power-system-tile"></button>
+								</div>
+								<div className="system-status-box">
+									<button className="thermal-system-tile"></button>
+								</div>
+								<div className="system-status-box">
+									<button className="water-system-tile"></button>
+								</div>
+								<div className="system-status-box">
+									<button className="compost-system-tile"></button>
+								</div>
 							</div>
 						</div>
 					</div>
