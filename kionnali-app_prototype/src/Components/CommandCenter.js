af7ba271911e294d1ce0fe645d039826be18f4a2
Selina Lavery <selina@kionnali.com>
2023-11-14 14:39:14 +0000

    mode tiles slide from side to side

--- kionnali-app_prototype/src/Components/CommandCenter.js
+++ kionnali-app_prototype/src/Components/CommandCenter.js
@@ -1,7 +1,7 @@
 import "../Components/CommandCenter.css";
 import yellowCircle from "../Assets/CommandCenter/yellowCircle.png";
-import StickyFooter from "./StickyFooter";
 import { useHistory } from "react-router-dom"; // if using react-router
+import ModeTiles from "./ModeTiles";
 
 export default function CommandCenter() {
 	let history = useHistory(); // if using react-router
@@ -14,11 +14,16 @@ export default function CommandCenter() {
 				<div className="command-center">
 					<h1>Command Center</h1>
 					<div className="cc-rounded-tile">
-						<img
-							src={yellowCircle}
-							alt=""
-							onClick={(e) => handleDashboardClick(e)}
-						/>
+						<div className="yellow-circle">
+							<img
+								src={yellowCircle}
+								alt=""
+								onClick={(e) => handleDashboardClick(e)}
+							/>
+						</div>
+						<div className="modes">
+							<ModeTiles />
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ kionnali-app_prototype/src/Components/ModeTiles.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ModeTiles.css";
+
+export default function ModeTiles() {
+	return (
+		<div className="slider">
+			<div className="mode-tile">
+				<p>1</p>
+			</div>
+			<div className="mode-tile">
+				<p>2</p>
+			</div>
+			<div className="mode-tile">
+				<p>3</p>
+			</div>
+			<div className="mode-tile">
+				<p>4</p>
+			</div>
+		</div>
+	);
+}
