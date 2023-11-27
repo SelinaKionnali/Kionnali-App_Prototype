62c2394f2832e116a7d126176500381ceafcacac
Selina Lavery <selina@kionnali.com>
2023-11-27 16:57:27 +0000

    installed react beautiful dnd and added id's to my data structure

--- kionnali-app_prototype/src/Components/MenuPageData.js
+++ kionnali-app_prototype/src/Components/MenuPageData.js
@@ -1,90 +1,113 @@
 const menuPageData = [
 	{
 		category: "Clean and Tidy",
+		categoryID: "cat-1",
 		subpages: [
 			{
 				page: "Organiser",
 				link: "/organiser",
+				id: "1",
 			},
 			{
 				page: "Shopping List",
 				link: "/shopping-list",
+				id: "2",
 			},
 			{
 				page: "Cleaning",
 				link: "/cleaning",
+				id: "3",
 			},
 		],
 	},
 	{
 		category: "Connect",
+		categoryID: "cat-2",
+
 		subpages: [
 			{
 				page: "Marketplace",
 				link: "/marketplace",
+				id: "4",
 			},
 			{
 				page: "Roamstead Chat",
 				link: "/roamstead-chat",
+				id: "5",
 			},
 			{
 				page: "Community Feed",
 				link: "/community-feed",
+				id: "6",
 			},
 			{
 				page: "Forum",
 				link: "/forum",
+				id: "7",
 			},
 		],
 	},
 	{
 		category: "Systems",
+		categoryID: "cat-3",
 		subpages: [
 			{
 				page: "Compare All",
 				link: "/compare-systems",
+				id: "8",
 			},
 			{
 				page: "Food",
 				link: "/food-system",
+				id: "9",
 			},
 			{
 				page: "Water",
 				link: "/water-system",
+				id: "10",
 			},
 			{
 				page: "Power",
 				link: "/power-system",
+				id: "11",
 			},
 			{
 				page: "Thermal",
 				link: "/thermal-system",
+				id: "12",
 			},
 			{
 				page: "Compost",
 				link: "/compost-system",
+				id: "13",
 			},
 			{
 				page: "EV",
 				link: "/ev-system",
+				id: "14",
 			},
 		],
 	},
 	{
 		category: "Weather",
+		categoryID: "cat-4",
+
 		subpages: [
 			{
 				page: "Weather",
 				link: "/weather",
+				id: "15",
 			},
 		],
 	},
 	{
 		category: "Preparedness",
+		categoryID: "cat-5",
 		subpages: [
 			{
 				page: "Preparedness",
 				link: "/preparedness",
+				id: "16",
 			},
 		],
 	},
