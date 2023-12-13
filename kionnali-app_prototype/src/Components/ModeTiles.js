e1027c224344aaab29fe206b3370a7fa910df790
SelinaKionnali <149412752+SelinaKionnali@users.noreply.github.com>
2023-12-13 10:14:54 +0000

    icons and images added

--- kionnali-app_prototype/src/Components/ModeTiles.js
+++ kionnali-app_prototype/src/Components/ModeTiles.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./ModeTiles.css";
 import Toggle from "./Toggle";
+import nightModeBed from "../Assets/CommandCenter/nightModeBed.png";
+import morningModeCoffee from "../Assets/CommandCenter/morningModeCoffee.png";
+import adventureModeMountain from "../Assets/CommandCenter/adventureModeMountain.png";
+import movieModePopcorn from "../Assets/CommandCenter/movieModePopcorn.png";
 
 export default function ModeTiles() {
 	const handleClick = (e) => {
@@ -19,30 +23,56 @@ export default function ModeTiles() {
 			</div>
 
 			<div className="mode-tile">
-				<div className="mode-toggle-wrapper">
-					<p>Night</p>
-					<Toggle />
+				<div className="tile-toggle-wrapper">
+					<img
+						src={nightModeBed}
+						alt="bed"
+					/>
+					<div className="mode-toggle-wrapper">
+						<p>Night</p>
+						<Toggle />
+					</div>
 				</div>
 			</div>
 
 			<div className="mode-tile">
-				<div className="mode-toggle-wrapper">
-					<p>Morning</p>
-					<Toggle />
+				<div className="tile-toggle-wrapper">
+					<img
+						src={morningModeCoffee}
+						alt="coffee cup"
+					/>
+					<div className="mode-toggle-wrapper">
+						<p>Morning</p>
+						<Toggle />
+					</div>
 				</div>
 			</div>
 
 			<div className="mode-tile">
-				<div className="mode-toggle-wrapper">
-					<p>Adventure</p>
-					<Toggle />
+				<div className="tile-toggle-wrapper">
+					<img
+						src={adventureModeMountain}
+						width="71"
+						height="73"
+						alt="Mountain yellow sky"
+					/>
+					<div className="mode-toggle-wrapper">
+						<p>Adventure</p>
+						<Toggle />
+					</div>
 				</div>
 			</div>
 
 			<div className="mode-tile">
-				<div className="mode-toggle-wrapper">
-					<p>Movies</p>
-					<Toggle />
+				<div className="tile-toggle-wrapper">
+					<img
+						src={movieModePopcorn}
+						alt="box of popcorn"
+					/>
+					<div className="mode-toggle-wrapper">
+						<p>Movies</p>
+						<Toggle />
+					</div>
 				</div>
 			</div>
 		</div>
