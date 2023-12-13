790b60e23a7bd2a9732c5b70326c609240245b96
SelinaKionnali <149412752+SelinaKionnali@users.noreply.github.com>
2023-12-13 09:06:08 +0000

    added the icon for the 'add mode' tile and handleClick

--- kionnali-app_prototype/src/Components/ModeTiles.js
+++ kionnali-app_prototype/src/Components/ModeTiles.js
@@ -3,26 +3,42 @@ import "./ModeTiles.css";
 import Toggle from "./Toggle";
 
 export default function ModeTiles() {
+	const handleClick = (e) => {
+		console.log("Add Mode button clicked");
+	};
+
 	return (
 		<div className="modes slider">
+			<div className="mode-tile">
+				<div className="add-mode-wrapper">
+					<button
+						className="add-mode"
+						onClick={handleClick}></button>
+					<p>Add Mode</p>
+				</div>
+			</div>
+
 			<div className="mode-tile">
 				<div className="mode-toggle-wrapper">
 					<p>Night</p>
 					<Toggle />
 				</div>
 			</div>
+
 			<div className="mode-tile">
 				<div className="mode-toggle-wrapper">
 					<p>Morning</p>
 					<Toggle />
 				</div>
 			</div>
+
 			<div className="mode-tile">
 				<div className="mode-toggle-wrapper">
 					<p>Adventure</p>
 					<Toggle />
 				</div>
 			</div>
+
 			<div className="mode-tile">
 				<div className="mode-toggle-wrapper">
 					<p>Movies</p>
