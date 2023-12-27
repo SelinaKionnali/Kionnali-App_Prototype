b2e4416f0512974ac37dc577726c83342b93ec2e
SelinaKionnali <149412752+SelinaKionnali@users.noreply.github.com>
2023-12-27 16:29:36 +0000

    Tiles added for Living Page in Command Center

--- kionnali-app_prototype/src/Components/LivingPage.js
+++ kionnali-app_prototype/src/Components/LivingPage.js
@@ -2,14 +2,28 @@ import "./TileComponents/TileLarge.css";
 import TileLarge from "./TileComponents/TileLarge";
 import TileNarrow from "./TileComponents/TileNarrow";
 import TileSmlSquare from "./TileComponents/TileSmlSquare";
+import ThreeDots from "../Assets/Dashboard/threeDots.png";
+import WattImg from "../Assets/CommandCenter/WattImg.png";
+import UsersPhoneImg from "../Assets/CommandCenter/UsersPhoneImg.png";
+import AllRoomsImg from "../Assets/CommandCenter/AllRoomsImg.png";
+import MusicTrackImg from "../Assets/CommandCenter/MusicTrackImg.png";
+import VolumeSliderImg from "../Assets/CommandCenter/VolumeSliderImg.png";
 
 export default function LivingPage() {
 	return (
 		<div className="tile-large-container">
-			<TileLarge />
+			<TileLarge
+				title1="Sound System"
+				watt1={WattImg}
+				img1={ThreeDots}
+				info1={UsersPhoneImg}
+				info2={AllRoomsImg}
+				content={MusicTrackImg}
+				slider={VolumeSliderImg}
+			/>
 			<TileSmlSquare
-				status1="Living Page"
-				status2="Living Page"
+				title1="Living Page"
+				title2="Living Page"
 			/>
 			<TileNarrow />
 			<TileLarge />
--- kionnali-app_prototype/src/Components/TileComponents/TileLarge.js
+++ kionnali-app_prototype/src/Components/TileComponents/TileLarge.js
@@ -1,5 +1,58 @@
 import "./TileLarge.css";
 
-export default function TileLarge() {
-	return <div className="large-tile"></div>;
+export default function TileLarge({
+	title1,
+	img1,
+	content,
+	info1,
+	info2,
+	slider,
+}) {
+	return (
+		<div
+			className="large-tile"
+			style={{ height: "60px" }}>
+			<div className="tile-heading">
+				<h3>{title1}</h3>
+				<img
+					src={img1}
+					alt="drag and drop icon"
+				/>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "flex-start",
+					width: "100%",
+					gap: "8px",
+				}}>
+				<img
+					src={info1}
+					alt=""
+				/>
+				<img
+					src={info2}
+					alt=""
+				/>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					width: "100%",
+					gap: "8px",
+				}}>
+				<img
+					src={content}
+					alt=""
+					style={{ width: "259px", height: "82px", margin: "32px 0 0 0" }}
+				/>
+				<img
+					src={slider}
+					alt=""
+				/>
+			</div>
+		</div>
+	);
 }
